a848cf1683f8cddc18a20afa52996e7efd6e9f9a
Alex Wu <alexdwu86@gmail.com>
2016-05-06 12:25:40 -0700

    better comments for 309

--- 309_BestTimeBuySellStockWithCooldown.js
+++ 309_BestTimeBuySellStockWithCooldown.js
@@ -21,17 +21,20 @@ var maxProfit = function(prices) {
   return prices.reduce(function(accum, price, index){
     // if this is one of the first three prices
     if (index < 3){
-      // buy the stock if the price is lower than what we previous
+      // set the buy price to be the lowest of the first three prices
       accum.buy = Math.min(price, accum.buy)
     } else {
-      // buy the stock if the price drops low enough
+      // do we want to buy at this price? 
+      // with the prevous(because cooldown) profit in mind
+      // (because making a profit effectively lowers out buy price), 
+      // compare buying at this price with the current buy price.
+      // buy at lowest price
       accum.buy = Math.min(price - accum.prev_profit, accum.buy);
     }
     // assign the previous profit to the current profit
     accum.prev_profit = accum.profit;
-    // sell if we make more $$$ than before
+    // if selling today means we make more money than if we sold yesterday
     accum.profit = Math.max(accum.profit, price - accum.buy);
-    console.log("index:", index, ", price:", price, accum);
     return accum;
   }, {
     buy: Infinity, // the price of the stock when we bought it
@@ -39,9 +42,9 @@ var maxProfit = function(prices) {
     prev_profit: 0 // the previous profit
   }).profit;
 };
-
 // Example
-prices = [-1, 2, -2, -2, 2, 50, 20, 30, 60, 100, 30]
+// prices = [1, 2, 3, 0, 2]
 // maxProfit = 3
 // transactions = [buy, sell, cooldown, buy, sell]
-console.log(maxProfit(prices));
+console.assert(maxProfit([1, 2, 3, 0, 2]) === 3, 'expect [1, 2, 3, 0, 2] to have max profit 3');
+console.assert(maxProfit([-1, 2, -2, -2, 2, 50, 20, 30, 60, 100, 30]) === 125, 'expect [-1, 2, -2, -2, 2, 50, 20, 30, 60, 100, 30] to have max profit 125');
